82d469759ccff14b50fc3fb8465cc5ceea3742ce
Krzysztof Stenografow <flamenco@gmail.com>
2023-06-27 18:03:41 +0200

    start podręcznika

---
+++ stemi-manual/README.md
@@ -0,0 +1,3 @@
+# Podręcznik do SteMi
+
+Tutaj będzie powstawał podręcznik do stenografii notatkowej SteMi. Ujęty będzie w wersję obszerną oraz skondensowaną, tj. dokument systemowy.
---
+++ stemi-manual/docs/source/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Podręcznik SteMi'
+copyright = '2023, Krzysztof Stenografow Smirnow'
+author = 'Krzysztof Stenografow Smirnow'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['myst_parser']
+
+# file extensions and their interpreters
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    #"linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'pl'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
